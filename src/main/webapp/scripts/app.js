224cc347436f4a1a8609f63573f849fb49aac01d
neo8ba8dms <blubrules@yahoo.de>
2015-12-28 03:27:46 +0100

    backend <--> frontend communication experiments

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('dmsApp', ['ui.router']);
+var app = angular.module('dmsApp', ['ui.router','ngResource']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
@@ -10,8 +10,25 @@ app.config(function($stateProvider, $urlRouterProvider) {
         })
         .state('documents', {
             url: '/documents',
-            templateUrl: 'html/documents.html'
+            templateUrl: 'html/documents.html',
+            controller: 'documentController'
         });
 
     $urlRouterProvider.otherwise('/home');
+});
+
+app.factory('documentService', function($resource){
+   return $resource('http://localhost:8080/documents/:id')
+});
+
+app.controller('documentController', function($scope, documentService){
+    var documents = documentService.query();
+    var document = documentService.get({id:2},function(){
+        console.log(document);
+
+    });
+
+    console.log(documents);
+
+
 });
