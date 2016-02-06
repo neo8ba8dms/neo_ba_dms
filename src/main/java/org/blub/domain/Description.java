199d9a7f3b4caf710589e6b7aeebb5f3396341d3
neo8ba8dms <blubrules@yahoo.de>
2016-02-06 01:54:44 +0100

    created Description class/entity

---
+++ src/main/java/org/blub/domain/Description.java
@@ -0,0 +1,66 @@
+package org.blub.domain;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+/*
+    Does this get a separate node in neo4J? --> yes
+    Needs a Relationship to classes that use it.
+    Naming scheme: "relFrom<using class>ToDescription"
+ */
+@NodeEntity
+public class Description {
+
+    @GraphId Long graphId;
+    private String textual_description;
+    private Description_enumeration_type type_of;
+    private String language_code;
+
+    public Long getGraphId() {
+        return graphId;
+    }
+
+    public void setGraphId(Long graphId) {
+        this.graphId = graphId;
+    }
+
+    public String getTextual_description() {
+        return textual_description;
+    }
+
+    public void setTextual_description(String textual_description) {
+        this.textual_description = textual_description;
+    }
+
+    public Description_enumeration_type getType_of() {
+        return type_of;
+    }
+
+    public void setType_of(Description_enumeration_type type_of) {
+        this.type_of = type_of;
+    }
+
+    public String getLanguage_code() {
+        return language_code;
+    }
+
+    public void setLanguage_code(String language_code) {
+        this.language_code = language_code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Description that = (Description) o;
+
+        return graphId != null ? graphId.equals(that.graphId) : that.graphId == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return graphId != null ? graphId.hashCode() : 0;
+    }
+}
