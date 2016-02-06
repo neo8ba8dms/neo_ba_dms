0dc48c55438e086ed966679ab62856b840b95050
neo8ba8dms <blubrules@yahoo.de>
2016-02-06 01:09:03 +0100

    extended data-model --- added all enums

---
+++ src/main/java/org/blub/domain/Description_enumeration_type.java
@@ -0,0 +1,12 @@
+package org.blub.domain;
+
+public enum Description_enumeration_type {
+    TITLE,
+    DESCRIPTION,
+    KEYWORD,
+    SUMMARY,
+    COMMENT,
+    PROPOSAL,
+    RESOLUTION,
+    EXPERIENCE
+}
---
+++ src/main/java/org/blub/domain/Document_relationship_type.java
@@ -0,0 +1,18 @@
+package org.blub.domain;
+
+public enum Document_relationship_type {
+    TRANSLATION,
+    REFERENCING,
+    VARIANT,
+    LANGUAGE_VARIANT,
+    COPY,
+    DECOMPOSITION,
+    SUBSTITUTION,
+    DERIVATION,
+    SUPERSEDING,
+    AFFECTING,
+    ADDITION,
+    PEER,
+    SEQUENCE,
+    SUPPLIED
+}
