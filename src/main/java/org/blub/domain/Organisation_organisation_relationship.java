c2817f94db75c9fe2e47605d05cee3355cade125
neo8ba8dms <blubrules@yahoo.de>
2016-02-28 21:58:34 +0100

    implemented organisation-organisation and organisation-person-relationships now have problems with cyclic dependencies in PUT and POST-requests

--- src/main/java/org/blub/domain/Organisation_organisation_relationship.java
+++ src/main/java/org/blub/domain/Organisation_organisation_relationship.java
@@ -1,10 +1,60 @@
 package org.blub.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.voodoodyne.jackson.jsog.JSOGGenerator;
+import org.neo4j.ogm.annotation.EndNode;
 import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
 
 @JsonIdentityInfo(generator=JSOGGenerator.class)
 @RelationshipEntity(type = "relFromOrganisationToOrganisation")
 public class Organisation_organisation_relationship extends Party_relationship{
+
+    @JsonIgnore
+    @StartNode
+    private Organisation relates_party;
+
+    @EndNode
+    private Organisation relating_party;
+
+    @Override
+    public Organisation getRelates_party() {
+        return relates_party;
+    }
+
+    public void setRelates_party(Organisation relates_party) {
+        this.relates_party = relates_party;
+    }
+
+    @Override
+    public Organisation getRelating_party() {
+        return relating_party;
+    }
+
+    public void setRelating_party(Organisation relating_party) {
+        this.relating_party = relating_party;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Organisation_organisation_relationship that = (Organisation_organisation_relationship) o;
+
+        if (relates_party != null ? !relates_party.equals(that.relates_party) : that.relates_party != null)
+            return false;
+        return relating_party != null ? relating_party.equals(that.relating_party) : that.relating_party == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (relates_party != null ? relates_party.hashCode() : 0);
+        result = 31 * result + (relating_party != null ? relating_party.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/webapp/scripts/organisationController.js
+++ src/main/webapp/scripts/organisationController.js
@@ -40,15 +40,84 @@ angular.module('dmsApp').controller('organisationController', function($scope, $
         });
     };
 
+    $scope.createNewOrganisationOrganisationRelationship = function(){
+        if(!$scope.organisation.organisation_organisation_relationships){
+            $scope.organisation.organisation_organisation_relationships = [];
+        }
+        if(!$scope.organisation.organisation_person_relationships){
+            $scope.organisation.organisation_person_relationships = [];
+        }
+
+        $scope.addOrganisationOrganisationRelationshipWithoutDuplicates($scope.organisation.organisation_organisation_relationships, $scope.tmpRelationship);
+        $scope.tmpRelationship = {};
+        $('#createNewOrganisationOrganisationRelationshipModal').modal('hide');
+    };
+
+    $scope.createNewOrganisationPersonRelationship = function(){
+        if(!$scope.organisation.organisation_organisation_relationships){
+            $scope.organisation.organisation_organisation_relationships = [];
+        }
+        if(!$scope.organisation.organisation_person_relationships){
+            $scope.organisation.organisation_person_relationships = [];
+        }
+
+        $scope.addOrganisationPersonRelationshipWithoutDuplicates($scope.organisation.organisation_person_relationships, $scope.tmpRelationship);
+        $scope.tmpRelationship = {};
+        $('#createNewOrganisationPersonRelationshipModal').modal('hide');
+    };
+
+    $scope.showOrganisationPersonRelationsModal = function(){
+        personService.query(function(persons){
+            $scope.persons = persons;
+        });
+    };
+
+
+    $scope.hideUpperModal = function(){
+        $('#createNewOrganisationOrganisationRelationshipModal').modal('hide');
+        $('#createNewOrganisationPersonRelationshipModal').modal('hide');
+
+    };
+
+    $scope.addOrganisationPersonRelationshipWithoutDuplicates = function(relationshipArray, tmpRelationship){
+        var found = false;
+        for(var i = 0; i < relationshipArray.length; i++){
+            if(relationshipArray[i].relating_party.first_name == tmpRelationship.relating_party.first_name
+                && relationshipArray[i].relating_party.last_name == tmpRelationship.relating_party.last_name){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            relationshipArray.push(tmpRelationship);
+        }
+    };
+
+    $scope.addOrganisationOrganisationRelationshipWithoutDuplicates = function(relationshipArray, tmpRelationship){
+        var found = false;
+        for(var i = 0; i < relationshipArray.length; i++){
+            if(relationshipArray[i].relating_party.name == tmpRelationship.relating_party.name){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            relationshipArray.push(tmpRelationship);
+        }
+    };
+
     //initial
     $scope.loadOrganisations();
 });
 
 /*********************************************************************************************************/
 
-angular.module('dmsApp').controller('organisationDetailsController', function($scope, $stateParams, organisationService) {
+angular.module('dmsApp').controller('organisationDetailsController', function($scope, $stateParams, organisationService, personService) {
 
     $scope.organisation = {};
+    $scope.organisations = [];
+    $scope.persons = [];
+    $scope.tmpRelationship = {};
 
     $scope.loadOrganisation = function(id){
         organisationService.get({id: id}, function(organisation){
@@ -57,7 +126,84 @@ angular.module('dmsApp').controller('organisationDetailsController', function($s
     };
 
     $scope.update = function(){
+        if(!$scope.organisation.organisation_organisation_relationships){
+            $scope.organisation.organisation_organisation_relationships = [];
+        }
+        if(!$scope.organisation.organisation_person_relationships){
+            $scope.organisation.organisation_person_relationships = [];
+        }
         organisationService.update($scope.organisation);
+    };
+
+    $scope.createNewOrganisationOrganisationRelationship = function(){
+        if(!$scope.organisation.organisation_organisation_relationships){
+            $scope.organisation.organisation_organisation_relationships = [];
+        }
+        if(!$scope.organisation.organisation_person_relationships){
+            $scope.organisation.organisation_person_relationships = [];
+        }
+
+        $scope.addOrganisationOrganisationRelationshipWithoutDuplicates($scope.organisation.organisation_organisation_relationships, $scope.tmpRelationship);
+        $scope.tmpRelationship = {};
+        $('#createNewOrganisationOrganisationRelationshipModal').modal('hide');
+    };
+
+    $scope.createNewOrganisationPersonRelationship = function(){
+        if(!$scope.organisation.organisation_organisation_relationships){
+            $scope.organisation.organisation_organisation_relationships = [];
+        }
+        if(!$scope.organisation.organisation_person_relationships){
+            $scope.organisation.organisation_person_relationships = [];
+        }
+
+        $scope.addOrganisationPersonRelationshipWithoutDuplicates($scope.organisation.organisation_person_relationships, $scope.tmpRelationship);
+        $scope.tmpRelationship = {};
+        $('#createNewOrganisationPersonRelationshipModal').modal('hide');
+    };
+
+    $scope.showOrganisationPersonRelationsModal = function(){
+        personService.query(function(persons){
+            $scope.persons = persons;
+        });
+    };
+
+    $scope.showOrganisationOrganisationRelationsModal = function(){
+        organisationService.query(function(organisations){
+            $scope.organisations = organisations;
+        });
+    };
+
+    $scope.hideUpperModal = function(){
+        $('#createNewOrganisationOrganisationRelationshipModal').modal('hide');
+        $('#createNewOrganisationPersonRelationshipModal').modal('hide');
+
+    };
+
+    $scope.addOrganisationPersonRelationshipWithoutDuplicates = function(relationshipArray, tmpRelationship){
+        var found = false;
+        for(var i = 0; i < relationshipArray.length; i++){
+            if(relationshipArray[i].relating_party.first_name == tmpRelationship.relating_party.first_name
+                && relationshipArray[i].relating_party.last_name == tmpRelationship.relating_party.last_name){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            relationshipArray.push(tmpRelationship);
+        }
+    };
+
+    $scope.addOrganisationOrganisationRelationshipWithoutDuplicates = function(relationshipArray, tmpRelationship){
+        var found = false;
+        for(var i = 0; i < relationshipArray.length; i++){
+            if(relationshipArray[i].relating_party.name == tmpRelationship.relating_party.name){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            relationshipArray.push(tmpRelationship);
+        }
     };
 
     //initial
