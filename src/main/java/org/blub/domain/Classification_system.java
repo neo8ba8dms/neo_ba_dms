b1e3ce49241db177b9723f22535ea3528fac01fa
neo8ba8dms <blubrules@yahoo.de>
2016-03-06 17:01:21 +0100

    fixed error, where updating a document with added classification-system lead to a SDN error, because there was no GraphId

--- src/main/java/org/blub/domain/Classification_system.java
+++ src/main/java/org/blub/domain/Classification_system.java
@@ -1,8 +1,21 @@
 package org.blub.domain;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
 public class Classification_system {
 
+    @GraphId Long graphId;
     private String id = "IEC 61355";
+
+    public Long getGraphId() {
+        return graphId;
+    }
+
+    public void setGraphId(Long graphId) {
+        this.graphId = graphId;
+    }
 
     public String getId() {
         return id;
@@ -19,12 +32,15 @@ public class Classification_system {
 
         Classification_system that = (Classification_system) o;
 
+        if (graphId != null ? !graphId.equals(that.graphId) : that.graphId != null) return false;
         return id != null ? id.equals(that.id) : that.id == null;
 
     }
 
     @Override
     public int hashCode() {
-        return id != null ? id.hashCode() : 0;
+        int result = graphId != null ? graphId.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
     }
 }
